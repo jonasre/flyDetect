fedec72b0272448feebe697ed1370c28765edc08
Jonas Paludan Reinholdt <jonaspr@student.matnat.uio.no>
2022-12-06 15:02:44 +0100

    Add detection of stable pressure

--- app/src/main/java/no/uio/ifi/jonaspr/flydetect/detectionservice/DecisionComponent.kt
+++ app/src/main/java/no/uio/ifi/jonaspr/flydetect/detectionservice/DecisionComponent.kt
@@ -3,17 +3,21 @@ package no.uio.ifi.jonaspr.flydetect.detectionservice
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.util.Log
+import kotlin.math.abs
 import kotlin.math.sqrt
 
 class DecisionComponent(accFrequency: Float, barFrequency: Float) {
     private var flying: Boolean = false
     private var roll: Boolean = false
     private var rollTimestamp: Long = 0L
+    private var stablePressure: Float = 0f
     private val accBuffer = SensorEventRingBuffer(accFrequency)
     private val barBuffer = SensorEventRingBuffer(barFrequency)
 
     private var nextAccCheckTime: Long = 0
     private var nextBarCheckTime: Long = 0
+
+    private var startTime = 0L
 
     private var nextCheckWindow: Int = (DEFAULT_CHECK_INTERVAL/1_000_000_000).toInt()
 
@@ -38,6 +42,7 @@ class DecisionComponent(accFrequency: Float, barFrequency: Float) {
     fun setStartTime(i: Long) {
         nextAccCheckTime = i + DEFAULT_CHECK_INTERVAL
         nextBarCheckTime = i + DEFAULT_CHECK_INTERVAL
+        startTime = i
     }
 
     private fun checkAcc() {
@@ -52,8 +57,8 @@ class DecisionComponent(accFrequency: Float, barFrequency: Float) {
             timeUntilNextCheck += window[0].timestamp - nextAccCheckTime
             Log.w(TAG, "Received empty moving average (gap in data?). Window: ${window.size}")
         }
-        if (ma.isNotEmpty())
-            Log.d(TAG, "checking from ${ma[0].first}, window: ${window.size}")
+        //if (ma.isNotEmpty())
+            //Log.d(TAG, "checking from ${ma[0].first}, window: ${window.size}")
         while (i < ma.size) {
             if (!flying && !roll) {
                 // Find first occurrence of acceleration withing takeoff roll range
@@ -174,8 +179,56 @@ class DecisionComponent(accFrequency: Float, barFrequency: Float) {
 
     private fun checkBar() {
         // ...
+        val window = barBuffer.getLatest(nextCheckWindow)
+        val variance = movingVariance(window, 10_000)
+        var timeUntilNextCheck = DEFAULT_CHECK_INTERVAL
 
-        nextBarCheckTime += DEFAULT_CHECK_INTERVAL
+        var i = 0
+        while (i < variance.size) {
+            // Loop until end of array or until pressure has left its stable level
+            while (
+                i < variance.size &&
+                abs(stablePressure - window[i].values[0]) < STABLE_PRESSURE_MIN_DIFF
+            ) i++
+            if (i >= variance.size) break // Stop if end of array was reached
+
+            var stableStartTime = 0L
+            // Loop until end of array or until first pressure under stable threshold is reached
+            while (i < variance.size) {
+                if (variance[i].second < STABLE_PRESSURE_THRESHOLD) {
+                    stableStartTime = variance[i].first
+                    break
+                }
+                i++
+            }
+
+            if (i >= variance.size) break
+
+            // Loop until end of array or as long as variance is below stable threshold
+            // Stop if variance is below stable threshold long enough to consider pressure as stable
+            while (i < variance.size && variance[i].second < STABLE_PRESSURE_THRESHOLD) {
+                if (stableStartTime + STABLE_PRESSURE_MIN_TIME <= variance[i].first) {
+                    // Stable pressure detected
+                    stablePressure = window[i].values[0]
+                    Log.i(TAG, "Stable pressure detected ($stablePressure at " +
+                            "${variance[i].first}, aka ${(variance[i].first-startTime)/1_000_000_000} s)")
+                    if (variance[i].first != window[i].timestamp)
+                        throw Exception("Timestamps in moving variance and window do not match")
+                    break
+                }
+                i++
+            }
+
+            if (i >= variance.size){
+                // End of array was reached while checking variance, schedule next check sooner
+                Log.d(TAG, "End of array was reached while checking variance, " +
+                        "scheduling next check sooner")
+                timeUntilNextCheck /= 2
+                break
+            }
+
+        }
+        nextBarCheckTime += timeUntilNextCheck
     }
 
     private fun movingAverage(source: Array<SensorEvent>, ms: Int): Array<Pair<Long, Float>> {
@@ -218,9 +271,57 @@ class DecisionComponent(accFrequency: Float, barFrequency: Float) {
         return arrayOf() // for the compiler
     }
 
+    private fun movingVariance(source: Array<SensorEvent>, ms: Int): Array<Pair<Long, Float>> {
+        if (source.isEmpty()) return arrayOf()
+        val mv = FloatArray(source.size)
+        if (source[0].sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            // Calculate magnitude
+            for (i in source.indices) {
+                mv[i] = source[i].values.let { v -> sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]) }
+            }
+        } else {
+            // sensor type is pressure
+            for (i in source.indices) mv[i] = source[i].values[0]
+        }
+
+        val windowSize: Long = ms*1_000_000L
+        var sum = 0f
+        var top = 0
+        var current = 0f
+        val highestTimestamp = source[source.lastIndex].timestamp
+        for (i in source.indices) {
+            sum -= current
+            current = mv[i]
+            val ceil = source[i].timestamp + windowSize
+            if (ceil > highestTimestamp) {
+                // Return when there isn't enough events to calculate the average.
+                // This is used to avoid noise at the end of the array
+                return Array(i) {
+                    Pair(source[it].timestamp, mv[it])
+                }
+            }
+            // Find top index for window
+            while (top < source.size && source[top].timestamp < ceil) {
+                // add to the sum as we go
+                sum += mv[top]
+                top++
+            }
+            val mean = sum / (top - i)
+            var total = 0f
+            for (j in i..top) {
+                val deviation = mean - mv[j]
+                total += deviation * deviation
+            }
+            mv[i] = total/(top-i)
+        }
+        return arrayOf() // for the compiler
+    }
+
     companion object {
         private const val TAG = "DC"
         private const val DEFAULT_CHECK_INTERVAL = 60_000_000_000 //nanoseconds (ns)
+
+        /* Acceleration related constants */
         private const val MOVING_AVG_WINDOW_SIZE = 10_000 //milliseconds (ms)
         private const val TAKEOFF_ROLL_TIME_MIN = 20_000_000_000 //nanoseconds (ns)
         private const val LIFTOFF_TIME_MIN = 5_000_000_000 //nanoseconds (ns)
@@ -228,5 +329,10 @@ class DecisionComponent(accFrequency: Float, barFrequency: Float) {
         private const val MIN_EVENTS_LIFTOFF = (LIFTOFF_TIME_MIN/1_000_000_000) * 5
         private val TAKEOFF_ROLL_ACC_RANGE = 9.95..10.3 // m/s^2
         private val LIFTOFF_ACC_RANGE = 10.8..11.4 // m/s^2
+
+        /* Pressure related constants */
+        private const val STABLE_PRESSURE_THRESHOLD = 0.004f //variance
+        private const val STABLE_PRESSURE_MIN_TIME = 10_000_000_000 //nanoseconds (ns)
+        private const val STABLE_PRESSURE_MIN_DIFF = 3f //hPa
     }
 }
